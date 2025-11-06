7f4d8cf008d0d12888559e899fa5ece1462e028a
RomanEntertainmentSoftwareLLC <romanentertainmentsoftware@gmail.com>
2025-11-05 20:34:53 -0500

    Fixed gap between the logo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -144,7 +144,7 @@ export default function Home() {
     return (
         <div className="mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8">
             {/* LOGO */}
-            <section className="pt-3 sm:pt-5">
+            <section className="pt-2 sm:pt-4 md:pt-6">
                 <Reveal className="text-center">
                     <img
                         src={dark ? ysongTitleWithLogoDark : ysongTitleWithLogo}
